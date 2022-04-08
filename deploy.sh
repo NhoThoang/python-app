7b2407ca7cf94d25200d28468e8c5d76228eca8f
Nguyễn Phạm Hoàng <xzhoang@pnh.com.vn>
2022-04-08 12:33:39 +0000

    update 9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 
 echo 'start deploy=====>'
-sudo docker pull registry.gitlab.com/xzhoang/pythonapp:latest
 cd $HOME/pythonapp
-git clone https://gitlab.com/xzhoang/pythonapp.git
+git clone https://gitlab.com/xzhoang/pythonapp.git .
+sudo docker-compose up -d
 echo '=====>deploy success'
