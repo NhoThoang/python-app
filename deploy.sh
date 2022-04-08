2319bc8de17fcb4ae43e68d6758705e210ac9d2e
Nguyễn Phạm Hoàng <xzhoang@pnh.com.vn>
2022-04-08 13:45:13 +0000

    update docker-compose

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ rm -fr $HOME/pythonapp
 mkdir -p $HOME/pythonapp
 cd $HOME/pythonapp
 git clone https://gitlab.com/xzhoang/pythonapp.git .
+sudo docker-compose down
 sudo docker-compose up -d
 echo '=====>deploy success'
