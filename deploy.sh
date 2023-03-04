5f29218534548e975c7db5294570915d05fa2376
NhoThoang <52397733+NhoThoang@users.noreply.github.com>
2023-03-04 07:54:09 +0700

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-
-echo 'deploy app to server Test: =======>'
-rm -fr $HOME/pythonapp
-mkdir -p $HOME/pythonapp
-cd $HOME/pythonapp
-git clone https://gitlab.com/xzhoang/pythonapp.git .
-sudo docker-compose down
-sudo docker-compose up -d
-echo '=====> deploy success on Test server'
